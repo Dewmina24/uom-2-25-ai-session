a2de646514bb7778d59f45e1f83621c7c8d41b96
Dewmina24 <dewminawijesinghe619@gmail.com>
2025-09-21 13:38:17 +0530

    Create Q3.py

---
+++ Project Euler/Q3.py
@@ -0,0 +1,11 @@
+#What is the largest prime factor of the number 600851475143 ?
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
+
+print(largest_prime_factor(600851475143))
